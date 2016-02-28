e4ca3559995e193992245f2f032887b73a59c89e
Stephanie Munch Jensen <s.munch.jensen@gmail.com>
2016-02-28 20:19:43 +0100

    Configured entry-point in package.json

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -60,8 +60,10 @@ var GoogleMap = {
 
       // How you would like to style the map.
       // This is where you would paste any style found on Snazzy Maps.
+      /*eslint-disable */
       //styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#dde6e8"},{"visibility":"on"}]}]
       styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"administrative.locality","elementType":"labels","stylers":[{"visibility":"on"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"},{"visibility":"simplified"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"visibility":"simplified"},{"saturation":"-65"},{"lightness":"45"},{"gamma":"1.78"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"on"}]},{"featureType":"road","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.highway","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"saturation":"-33"},{"lightness":"22"},{"gamma":"2.08"}]},{"featureType":"transit.station.airport","elementType":"geometry","stylers":[{"gamma":"2.08"},{"hue":"#ffa200"}]},{"featureType":"transit.station.airport","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit.station.rail","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"transit.station.rail","elementType":"labels.icon","stylers":[{"visibility":"simplified"},{"saturation":"-55"},{"lightness":"-2"},{"gamma":"1.88"},{"hue":"#ffab00"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#bbd9e5"},{"visibility":"simplified"}]}]
+      /*eslint-enable */
     };
 
     // Get the HTML DOM element that will contain your map
@@ -86,13 +88,13 @@ var GoogleMap = {
       title: 'Rockefeller Music Hall'
     });
 
-    var contentString = '<div id="map-infowindow">'+
-      '<div id="bodyContent">'+
-        '<a href="http://www.dansenshus.com/" target="_blank">'+
-          '<img src="http://norskedansekunstnere.no/wp-content/uploads/2014/06/dhlogo.png" alt="Dansens Hus" />'+
-        '</a>'+
-        '<p><strong>Dansens Hus</strong><br/>Møllerveien 2<br/>0182 Oslo, Norway</p>'+
-      '</div>'+
+    var contentString = '<div id="map-infowindow">' +
+      '<div id="bodyContent">' +
+        '<a href="http://www.dansenshus.com/" target="_blank">' +
+          '<img src="http://norskedansekunstnere.no/wp-content/uploads/2014/06/dhlogo.png" alt="Dansens Hus" />' +
+        '</a>' +
+        '<p><strong>Dansens Hus</strong><br/>Møllerveien 2<br/>0182 Oslo, Norway</p>' +
+      '</div>' +
       '</div>';
 
   var infowindow = new google.maps.InfoWindow({
